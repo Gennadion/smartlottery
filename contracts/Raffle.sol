e1bc87dfcc0c57838dfc80f6d5b9f8f3819aeb7d
Gennadion <ghammurti@gmail.com>
2022-10-11 00:25:32 +0300

    Added basic functions and events to Raffle.sol

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,4 +8,36 @@
 
 pragma solidity ^0.8.17;
 
-contract Raffle {}
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+	/* State Variables */
+	uint256 private immutable i_entranceFee;
+	address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+	constructor(uint256 entranceFee) {
+		i_entranceFee = entranceFee;
+	}
+
+	function enterRaffle() public payable {
+		if (msg.value < i_entranceFee) {
+			revert Raffle__NotEnoughETHEntered();
+		}
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEnter(msg.sender);
+	}
+
+	// function pickRandomWinner() {}
+
+	function getEntranceFee() public view returns (uint256) {
+		return i_entranceFee;
+	}
+
+	function getPlayer(uint256 index) public view returns (address) {
+		return s_players[index];
+	}
+}
