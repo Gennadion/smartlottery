9ad996d7dd0259185188c4f5b550126ba58dbc62
Gennadion <ghammurti@gmail.com>
2022-10-10 21:42:11 +0300

    Added dependencies and created Raffle contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,11 @@
+// Raffle
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completely automated
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract Raffle {}
