33b50cd32cf5ad7c3a155cc9ffcdefdf18a24553
Gennadion <ghammurti@gmail.com>
2022-10-23 16:05:52 +0300

    Initialized deploy scripts

---
+++ helper-hardhat.config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+	5: {
+		name: "goerli",
+		vrfCoordinatorV2: "0x2bce784e69d2ff36c71edcb9f88358db0dfb55b4",
+	},
+};
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
